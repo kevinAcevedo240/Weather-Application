a5126cedb2959f2621217f92728c77365c870b7e
Kevin Julian Acevedo Salazar <kjacevedos@outsourcing.com.co>
2023-07-17 12:43:12 -0500

    Update:dynamic landscape background and current date

--- src/app/modules/dashboard/components/main-card/main-card.component.ts
+++ src/app/modules/dashboard/components/main-card/main-card.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./main-card.component.scss']
 })
 export class MainCardComponent {
+  currentHour: number;
+  currentDate: string;
 
+  constructor() {
+    this.currentHour = new Date().getHours();
+    this.currentDate = this.formatDate(new Date());
+  }
+
+  formatDate(date: Date): string {
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric'
+    };
+
+    return date.toLocaleDateString('en-US', options);
+  }
+
+  getCardClass(): string {
+    console.log(this.currentHour);
+    if (this.currentHour >= 8 && this.currentHour < 17) {
+      return 'morning';
+    } else if (this.currentHour >= 17 && this.currentHour < 19) {
+      return 'sunrise';
+    } else if (this.currentHour >= 19 && this.currentHour < 21) {
+      return 'mid-nightfall';
+    } else if (this.currentHour >= 21 && this.currentHour < 24) {
+      return 'nightfall';
+    } else {
+      return 'sunset';
+    }
+
+
+  }
 }
